301df68a7c40cbb6907770f1aaf8e1e9ec8f2b1e
nauraputri25 <002nauranaura@gmail.com>
2026-03-31 14:55:38 +0700

    Fix: Define user variable and save file

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,16 +1,18 @@
+const jwt = require("jsonwebtoken");
+
 const token = jwt.sign(
-    { 
-      id: user._id, 
-      role: user.role, 
-      isVerified: user.isVerified, 
-      name: user.name 
-    }, 
-    process.env.JWT_SECRET, 
-    { expiresIn: '1d' }
+  {
+    id: user._id,
+    role: user.role,
+    isVerified: user.isVerified,
+    name: user.name,
+  },
+  process.env.JWT_SECRET,
+  { expiresIn: "1d" },
 );
 
 if (user.role === "perusahaan" && user.isVerified === false) {
-   return res.render("login", {
-      error: "Akun perusahaan anda sedang menunggu verifikasi admin"
-   });
+  return res.render("login", {
+    error: "Akun perusahaan anda sedang menunggu verifikasi admin",
+  });
 }
